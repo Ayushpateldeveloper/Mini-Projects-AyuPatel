5c6a5e7db50a71fe50dc7a0c8ccbd61345272156
Ayush Patel <105097341+Ayupatel2310@users.noreply.github.com>
2023-06-09 19:01:13 +0530

    javascript file

---
+++ script.js
@@ -0,0 +1,3 @@
+  document.getElementById('code-btn').addEventListener('click', function() {
+    window.location.href = 'https://github.com/Ayupatel2310/Mini-Projects-AyuPatel/tree/main/Add%20To%20Cart%20Interaction'; // Replace with your GitHub page URL
+  });
